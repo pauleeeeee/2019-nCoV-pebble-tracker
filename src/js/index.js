f11149ce273e4fd9033f7350bdc21f35197f5392
pauleeeeee <paul.embleton@gmail.com>
2020-03-25 12:06:17 -0600

    v2.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
-const moment = require('moment');
-const Papa = require('papaparse');
+// const moment = require('moment');
+// const Papa = require('papaparse');
+
+//require Clay for settings
 var Clay = require('pebble-clay');
 var clayConfig = require('./config.json');
 var clay = new Clay(clayConfig, null, { autoHandleEvents: false });
+
+//require message queueing system
+var MessageQueue = require('message-queue-pebble');
 
 var yourLocation = {
     lat: 0,
@@ -25,12 +30,14 @@ Pebble.addEventListener('webviewclosed', function(e) {
     if (e && !e.response) {
         return;
     }
+    console.log('web view closed');
     var configuration = clay.getSettings(e.response, false);
-    if (configuration.CustomLat.value && configuration.CustomLat.value != "" & configuration.CustomLong.value && configuration.CustomLong.value != "") {
+    if (configuration.CustomLat.value && configuration.CustomLat.value != "" && configuration.CustomLong.value && configuration.CustomLong.value != "") {
         yourLocation.lat = configuration.CustomLat.value;
         yourLocation.long = configuration.CustomLong.value;
         customLocation = true;
         localStorage.setItem("customLocation", JSON.stringify(yourLocation));
+        console.log('custom location detected and set');
     } else {
         customLocation = false;
         localStorage.removeItem("customLocation");
@@ -45,7 +52,7 @@ Pebble.addEventListener('webviewclosed', function(e) {
 
 Pebble.addEventListener('ready', function() {
     lastSync = localStorage.getItem("lastSync");
-    var customLocation = localStorage.getItem("customLocation");
+    var customLocation = JSON.parse(localStorage.getItem("customLocation"));
     if (customLocation){
         yourLocation = customLocation;
         customLocation = true;
@@ -104,83 +111,184 @@ function fetchAll(){
 }
 
 function getCoronaData(){
-    Papa.parse('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
-    {
-        download:true,
-        complete:(results)=>{
-            var modified = [];
-            for (let location of results.data){
-                var count = location[location.length-1];
-                if (count>minCases){
-                    var locString = "";
-                    if(location[0].length > 1){
-                        locString = location[0] + ", " + location[1];
-                    } else {
-                        locString = location[1];
+    var req = new XMLHttpRequest();
+    req.open('GET', "https://corona.lmao.ninja/jhucsse", true);
+    req.onload = function(e) {
+        if (req.readyState == 4) {
+          // 200 - HTTP OK
+            if(req.status == 200) {
+                var locations = JSON.parse(req.responseText);
+
+                //example location entry returned by the API
+                //{"country":"China",
+                //"province":"Hubei",
+                //"updatedAt":"2020-03-21T10:13:08",
+                //"stats":{"confirmed":"67800","deaths":"3139","recovered":"58946"},
+                //"coordinates":{"latitude":"30.9756","longitude":"112.2707"}}
+
+                //world stats
+                var worldStats = {
+                    "Location": "World",
+                    "Cases": 0,
+                    "Deaths": 0,
+                    "Recovered": 0,
+                    "Scope": 2
+                };
+
+
+                for (let location of locations){
+                    //calculate distance
+                    location.distance = Math.round(distance(location.coordinates.latitude, location.coordinates.longitude,  yourLocation.lat, yourLocation.long, units));
+                    
+                    //add to world stats
+                    worldStats.Cases += Number(location.stats.confirmed);
+                    worldStats.Deaths += Number(location.stats.deaths);
+                    worldStats.Recovered += Number(location.stats.recovered);
+                 }
+
+                 worldStats.Cases += "";
+                 worldStats.Deaths += "";
+                 worldStats.Recovered += "";
+
+                //sort locations by distance from current location, ascending
+                locations.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
+                // console.log(JSON.stringify(locations[0]));
+                // console.log(worldStats);
+                var regionalStats = {
+                    "Location": "",
+                    // "Cases":locations[0].stats.confirmed+"",
+                    // "Deaths":locations[0].stats.deaths+"",
+                    // "Recovered":locations[0].stats.recovered+"",
+                    "Cases":0,
+                    "Deaths":0,
+                    "Recovered":0,
+                    "Scope": 0
+                };
+
+                //check to see if a county is not listed with provinces. If it is not, report both regional and country as country
+                var countryString;
+                var countryStats;
+                if (locations[0].province == null || locations[0].province == locations[0].country){
+                    countryStats = {
+                        "Location": locations[0].country,
+                        "Cases":locations[0].stats.confirmed+"",
+                        "Deaths":locations[0].stats.deaths+"",
+                        "Recovered":locations[0].stats.recovered+"",
+                        "Scope": 1
                     }
-                    var loc = {
-                        location:locString,
-                        lat: location[2],
-                        long: location[3],
-                        distance: Math.round(distance(location[2], location[3],  yourLocation.lat, yourLocation.long, units)),
-                        count:count
+                    regionalStats.Location = locations[0].country;
+                } else if (locations[0].province != locations[0].country && locations[0].province != null){
+                    regionalStats.Location = locations[0].province;
+                    countryString = locations[0].country;
+                    for (let location of locations){
+                        if (location.province == regionalStats.Location){
+                            console.log(JSON.stringify(location));
+                            regionalStats.Cases += Number(location.stats.confirmed);
+                            regionalStats.Deaths += Number(location.stats.deaths);
+                            regionalStats.Recovered += Number(location.stats.recovered);
+                        }
                     }
-                    if(!locString.includes("Diamond Princess")){
-                        modified.push(loc);
+                    regionalStats.Cases += "";
+                    regionalStats.Deaths += "";
+                    regionalStats.Recovered += "";
+   
+                }
+                
+                //if country stats hasn't been established, build the object by adding all provinces together
+                if (!countryStats) {
+                    var stats = {
+                        "confirmed":0,
+                        "deaths":0,
+                        "recovered":0
+                    }
+                    for (let location of locations){
+                        if (location.country == countryString){
+                            stats.confirmed += Number(location.stats.confirmed);
+                            stats.deaths += Number(location.stats.deaths);
+                            stats.recovered += Number(location.stats.recovered);
+                        }
+                    }
+                    countryStats = {
+                        "Location": countryString,
+                        "Cases":stats.confirmed+"",
+                        "Deaths":stats.deaths+"",
+                        "Recovered":stats.recovered+"",
+                        "Scope": 1
                     }
                 }
+
+                console.log(JSON.stringify(worldStats));
+                console.log(JSON.stringify(countryStats));
+                console.log(JSON.stringify(regionalStats));
+                MessageQueue.sendAppMessage(regionalStats);
+                MessageQueue.sendAppMessage(countryStats);
+                MessageQueue.sendAppMessage(worldStats);
+                MessageQueue.sendAppMessage({"Ready":1});
+
             }
-            modified.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
-            console.log(JSON.stringify(modified[0]));
-            var closestLocations = "";
-            for (let i=0; i < 5; i++){
-                closestLocations += modified[i].location + "\n" + modified[i].distance+displayUnits(units)+", " + modified[i].count + " cases" + "\n" ;
-            }
-            var virusData = {
-                "Distance": modified[0].distance+displayUnits(units),
-                "Location": modified[0].location,
-                "LocationCases": modified[0].count + " cases",
-                "ClosestCases": closestLocations
-            };
-            // localStorage.setItem("lastSync", moment().format("MM-DD-YYYY"));
-            localStorage.setItem("lastSync", new Date().getTime());
-            localStorage.setItem("virusData", JSON.stringify(virusData));
-            Pebble.sendAppMessage(virusData);
-            // console.log(JSON.stringify(modified));
-        },
-        error:()=>{
-            Pebble.showSimpleNotificationOnPebble("Error", "Error in parsing results from webserver.");
         }
-    });
+    }
+    req.send();
+
 }
 
-//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//:::                                                                         :::
-//:::  This routine calculates the distance between two points (given the     :::
-//:::  latitude/longitude of those points). It is being used to calculate     :::
-//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
-//:::                                                                         :::
-//:::  Definitions:                                                           :::
-//:::    South latitudes are negative, east longitudes are positive           :::
-//:::                                                                         :::
-//:::  Passed to function:                                                    :::
-//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
-//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
-//:::    unit = the unit you desire for results                               :::
-//:::           where: 'M' is statute miles (default)                         :::
-//:::                  'K' is kilometers                                      :::
-//:::                  'N' is nautical miles                                  :::
-//:::                                                                         :::
-//:::  Worldwide cities and other features databases with latitude longitude  :::
-//:::  are available at https://www.geodatasource.com                         :::
-//:::                                                                         :::
-//:::  For enquiries, please contact sales@geodatasource.com                  :::
-//:::                                                                         :::
-//:::  Official Web site: https://www.geodatasource.com                       :::
-//:::                                                                         :::
-//:::               GeoDataSource.com (C) All Rights Reserved 2018            :::
-//:::                                                                         :::
-//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//old papaparse functionality
+// function getCoronaData2(){
+//     Papa.parse('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+//     {
+//         download:true,
+//         complete:(results)=>{
+//             var modified = [];
+//             for (let location of results.data){
+//                 var count = location[location.length-1];
+//                 if (count>minCases){
+//                     var locString = "";
+//                     if(location[0].length > 1){
+//                         locString = location[0] + ", " + location[1];
+//                     } else {
+//                         locString = location[1];
+//                     }
+//                     var loc = {
+//                         location:locString,
+//                         lat: location[2],
+//                         long: location[3],
+//                         distance: Math.round(distance(location[2], location[3],  yourLocation.lat, yourLocation.long, units)),
+//                         count:count
+//                     }
+//                     if(!locString.includes("Diamond Princess")){
+//                         modified.push(loc);
+//                     }
+//                 }
+//             }
+//             modified.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
+//             console.log(JSON.stringify(modified[0]));
+//             var closestLocations = "";
+//             for (let i=0; i < 5; i++){
+//                 closestLocations += modified[i].location + "\n" + modified[i].distance+displayUnits(units)+", " + modified[i].count + " cases" + "\n" ;
+//             }
+//             var virusData = {
+//                 // "Distance": modified[0].distance+displayUnits(units),
+//                 "Scope": 0,
+//                 "Location": modified[0].location,
+//                 "Cases": modified[0].count + "",
+//                 "Deaths": 0+" ",
+//                 "Recovered": 0+" ",
+//                 "Ready": 1,
+//                 // "ClosestCases": closestLocations
+//             };
+//             console.log(JSON.stringify(virusData));
+//             // localStorage.setItem("lastSync", moment().format("MM-DD-YYYY"));
+//             localStorage.setItem("lastSync", new Date().getTime());
+//             localStorage.setItem("virusData", JSON.stringify(virusData));
+//             Pebble.sendAppMessage(virusData);
+//             // console.log(JSON.stringify(modified));
+//         },
+//         error:()=>{
+//             Pebble.showSimpleNotificationOnPebble("Error", "Error in parsing results from webserver.");
+//         }
+//     });
+// }
+
 
 function distance(lat1, lon1, lat2, lon2, unit) {
 	if ((lat1 == lat2) && (lon1 == lon2)) {
