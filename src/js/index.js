963eeb5a724b9241a4173dd4b84be5b6710005e9
pauleeeeee <paul.embleton@gmail.com>
2020-03-27 13:53:11 -0600

    removed reverse geocoding; now supports iOS

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,3 @@
-// const moment = require('moment');
-// const Papa = require('papaparse');
-const point2place = require('point2place');
-
 //require Clay for settings
 var Clay = require('pebble-clay');
 var clayConfig = require('./config.json');
@@ -10,16 +6,10 @@ var clay = new Clay(clayConfig, null, { autoHandleEvents: false });
 //require message queueing system
 var MessageQueue = require('message-queue-pebble');
 
-var yourLocation = {
-    lat: 0,
-    long: 0
-}
-
-var lastSync = "";
-var useCustomLocation = false;
+var country = '';
+var stateProvince = '';
 
 // localStorage.clear();
-
 
 Pebble.addEventListener('showConfiguration', function(e) {
     Pebble.openURL(clay.generateUrl());
@@ -31,39 +21,26 @@ Pebble.addEventListener('webviewclosed', function(e) {
     }
     console.log('web view closed');
     var configuration = clay.getSettings(e.response, false);
-    console.log(JSON.stringify(configuration))
-
-    if (configuration.CustomLat.value != "" && configuration.CustomLong.value != "") {
-        yourLocation.lat = configuration.CustomLat.value;
-        yourLocation.long = configuration.CustomLong.value;
-        useCustomLocation = true;
-        localStorage.setItem("customLocation", JSON.stringify(yourLocation));
-        // console.log('custom location detected and set');
-    } else {
-        useCustomLocation = false;
-        localStorage.removeItem("customLocation");
-    }
+    // console.log(JSON.stringify(configuration));
+    localStorage.setItem("configuration", JSON.stringify(configuration));
+    country = configuration.Country.value;
+    stateProvince = configuration.StateProvince.value;
     fetchAll();
 });
 
 Pebble.addEventListener('ready', function() {
     // lastSync = localStorage.getItem("lastSync");
-    var customLocation = JSON.parse(localStorage.getItem("customLocation"));
-    console.log(JSON.stringify(customLocation));
-    if (customLocation){
-        // console.log('custom location detected in ready listener');
-        yourLocation = customLocation;
-        useCustomLocation = true;
-    };
-    if (lastSync == "" || (new Date().getTime() - lastSync) > 3.6e+6 ){
-        fetchAll();
-        // console.log('fetching fresh');
+    var configuration = JSON.parse(localStorage.getItem("configuration"));
+    // console.log(JSON.stringify(configuration));
+    if (configuration){
+        country = configuration.Country.value;
+        stateProvince = configuration.StateProvince.value;
     } else {
-        var virusData = JSON.parse(localStorage.getItem("virusData"));
-        Pebble.sendAppMessage(virusData);
-        // console.log('sent cached');
-    };
-
+        country = "USA";
+        stateProvince = "California";
+        Pebble.showSimpleNotificationOnPebble("Configuration needed", "Visit the app's settings page inside the Pebble phone app to configure your location.");
+    }
+    fetchAll();
 });
 
 Pebble.addEventListener('appmessage', function(e) {
@@ -73,51 +50,18 @@ Pebble.addEventListener('appmessage', function(e) {
     }
 });
 
-function success(pos) {
-    yourLocation.lat = pos.coords.latitude;
-    yourLocation.long = pos.coords.longitude;
-    getCoronaData();
-}
-
-function error(err) {
-    console.log('location error (' + err.code + '): ' + err.message);
-    Pebble.showSimpleNotificationOnPebble("Error", "Geolocation fetch failed.");
-}
-
-// Choose options about the data returned
-var options = {
-    enableHighAccuracy: true,
-    timeout: 10000
-};
 
 function fetchAll(){
-    if (useCustomLocation) {
-        // console.log('custom location caught in fetchAll');
-        getCoronaData();
-    } else {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    }
-}
-
-var place;
-
-function getCoronaData(){
-    place = point2place([yourLocation.long, yourLocation.lat]);
-    // console.log(JSON.stringify(place));
-    // var example = {
-    //     "country":{"id":"USA","continent":"North America","name":"United States of America","name2":"United States","population":326625791,"income":"1. High income: OECD"},
-    //     "nearestCity":{"name":"Denver","state_province":"Colorado","population":2313000,"sovereign":"United States of America","distanceKm":17.629558350620453}
-    // };
-   getWorld();
+    getWorld();
 }
 
 function getWorld(){
     var worldStats = {
-        "Location": "World",
-        "Cases": 0,
-        "Deaths": 0,
-        "Recovered": 0,
-        "Scope": 2
+        'Location': 'World',
+        'Cases': 0,
+        'Deaths': 0,
+        'Recovered': 0,
+        'Scope': 2
     };
     var req = new XMLHttpRequest();
     req.open('GET', "https://corona.lmao.ninja/all", true);
@@ -130,7 +74,9 @@ function getWorld(){
                 worldStats.Cases = worldData.cases + "";
                 worldStats.Deaths = worldData.deaths + "";
                 worldStats.Recovered = worldData.recovered + "";
-                MessageQueue.sendAppMessage(worldStats);
+                console.log('sending world');
+                console.log(JSON.stringify(worldStats));
+                MessageQueue.sendAppMessage(worldStats, onSuccess, onFailure);
                 getCountry();
             }
         }
@@ -138,9 +84,21 @@ function getWorld(){
     req.send();
 };
 
+function onSuccess(data){
+    console.log('success');
+    console.log(JSON.stringify(data));
+}
+
+function onFailure(data, error){
+    console.log('error');
+    console.log(JSON.stringify(data));
+    console.log(JSON.stringify(error));
+
+}
+
 function getCountry(){
     var req = new XMLHttpRequest();
-    req.open('GET', "https://corona.lmao.ninja/countries/" + place.country.name, true);
+    req.open('GET', "https://corona.lmao.ninja/countries/" + country, true);
     req.onload = function(e) {
         if (req.readyState == 4) {
           // 200 - HTTP OK
@@ -153,24 +111,24 @@ function getCountry(){
                 //   "cases":65652,"todayCases":10796,"deaths":931,"todayDeaths":151,"recovered":394,"active":64327,"critical":1411,"casesPerOneMillion":198,"deathsPerOneMillion":3
                 // }
                 var countryData = {
-                    "Location": place.country.name,
-                    "Cases": data.cases + "",
-                    "Deaths": data.deaths + "",
-                    "Recovered": data.recovered + "",
-                    "Scope": 1
-                }
+                    'Location': country,
+                    'Cases': data.cases + "",
+                    'Deaths': data.deaths + "",
+                    'Recovered': data.recovered + "",
+                    'Scope': 1
+                };
+                console.log('sending country');
+                console.log(JSON.stringify(countryData));
                 MessageQueue.sendAppMessage(countryData);
-                if (place.country.id == "USA") {
+                if (country == "USA") {
                     getState();
                 } else {
-                    MessageQueue.sendAppMessage({"Ready":2})
+                    MessageQueue.sendAppMessage({'Ready':2})
                 }
             }
         }
     }
     req.send();
-
-
 };
 
 function getState(){
@@ -183,234 +141,27 @@ function getState(){
                 var states = JSON.parse(req.responseText);
                 var stateInfo;
                 //{"state":"Colorado","cases":1086,"todayCases":174,"deaths":19,"todayDeaths":8,"active":1067}
-                for (let state of states){
-                    if (state.state == place.nearestCity.state_province){
-                        stateInfo = state;
+                for ( i = 0; i < states.length; i++){
+                    if (states[i].state == stateProvince){
+                        stateInfo = states[i];
                         break;
                     }
                 }
                 var stateData = {
-                    "Location": stateInfo.state,
-                    "Cases": stateInfo.cases + "",
-                    "Deaths": stateInfo.deaths + "",
-                    "Recovered": "?" + "",
-                    "Scope": 0
-                }
+                    'Location': stateInfo.state,
+                    'Cases': stateInfo.cases + "",
+                    'Deaths': stateInfo.deaths + "",
+                    'Recovered': "?" + "",
+                    'Scope': 0
+                };
+                console.log('sending state');
+                console.log(JSON.stringify(stateData));
+
                 MessageQueue.sendAppMessage(stateData);
-                MessageQueue.sendAppMessage({"Ready":1})
+                MessageQueue.sendAppMessage({'Ready':1})
             }
         }
     }
     req.send();
 
 };
-
-// function getCoronaData3(){
-//     var req = new XMLHttpRequest();
-//     req.open('GET', "https://corona.lmao.ninja/jhucsse", true);
-//     req.onload = function(e) {
-//         if (req.readyState == 4) {
-//           // 200 - HTTP OK
-//             if(req.status == 200) {
-//                 var locations = JSON.parse(req.responseText);
-
-//                 //example location entry returned by the API
-//                 //{"country":"China",
-//                 //"province":"Hubei",
-//                 //"updatedAt":"2020-03-21T10:13:08",
-//                 //"stats":{"confirmed":"67800","deaths":"3139","recovered":"58946"},
-//                 //"coordinates":{"latitude":"30.9756","longitude":"112.2707"}}
-
-//                 //world stats
-//                 var worldStats = {
-//                     "Location": "World",
-//                     "Cases": 0,
-//                     "Deaths": 0,
-//                     "Recovered": 0,
-//                     "Scope": 2
-//                 };
-
-
-//                 for (let location of locations){
-//                     //calculate distance
-//                     location.distance = Math.round(distance(location.coordinates.latitude, location.coordinates.longitude,  yourLocation.lat, yourLocation.long, units));
-                    
-//                     //add to world stats
-//                     worldStats.Cases += Number(location.stats.confirmed);
-//                     worldStats.Deaths += Number(location.stats.deaths);
-//                     worldStats.Recovered += Number(location.stats.recovered);
-//                  }
-
-//                  worldStats.Cases += "";
-//                  worldStats.Deaths += "";
-//                  worldStats.Recovered += "";
-
-//                 //sort locations by distance from current location, ascending
-//                 locations.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
-//                 // console.log(JSON.stringify(locations[0]));
-//                 // console.log(worldStats);
-//                 var regionalStats = {
-//                     "Location": "",
-//                     // "Cases":locations[0].stats.confirmed+"",
-//                     // "Deaths":locations[0].stats.deaths+"",
-//                     // "Recovered":locations[0].stats.recovered+"",
-//                     "Cases":0,
-//                     "Deaths":0,
-//                     "Recovered":0,
-//                     "Scope": 0
-//                 };
-
-//                 //check to see if a county is not listed with provinces. If it is not, report both regional and country as country
-//                 var countryString;
-//                 var countryStats;
-//                 if (locations[0].province == null || locations[0].province == locations[0].country){
-//                     countryStats = {
-//                         "Location": locations[0].country,
-//                         "Cases":locations[0].stats.confirmed+"",
-//                         "Deaths":locations[0].stats.deaths+"",
-//                         "Recovered":locations[0].stats.recovered+"",
-//                         "Scope": 1
-//                     }
-//                     regionalStats.Location = locations[0].country;
-//                 } else if (locations[0].province != locations[0].country && locations[0].province != null){
-//                     regionalStats.Location = locations[0].province;
-//                     countryString = locations[0].country;
-//                     for (let location of locations){
-//                         if (location.province == regionalStats.Location){
-//                             console.log(JSON.stringify(location));
-//                             regionalStats.Cases += Number(location.stats.confirmed);
-//                             regionalStats.Deaths += Number(location.stats.deaths);
-//                             regionalStats.Recovered += Number(location.stats.recovered);
-//                         }
-//                     }
-//                     regionalStats.Cases += "";
-//                     regionalStats.Deaths += "";
-//                     regionalStats.Recovered += "";
-   
-//                 }
-                
-//                 //if country stats hasn't been established, build the object by adding all provinces together
-//                 if (!countryStats) {
-//                     var stats = {
-//                         "confirmed":0,
-//                         "deaths":0,
-//                         "recovered":0
-//                     }
-//                     for (let location of locations){
-//                         if (location.country == countryString){
-//                             stats.confirmed += Number(location.stats.confirmed);
-//                             stats.deaths += Number(location.stats.deaths);
-//                             stats.recovered += Number(location.stats.recovered);
-//                         }
-//                     }
-//                     countryStats = {
-//                         "Location": countryString,
-//                         "Cases":stats.confirmed+"",
-//                         "Deaths":stats.deaths+"",
-//                         "Recovered":stats.recovered+"",
-//                         "Scope": 1
-//                     }
-//                 }
-
-//                 console.log(JSON.stringify(worldStats));
-//                 console.log(JSON.stringify(countryStats));
-//                 console.log(JSON.stringify(regionalStats));
-//                 MessageQueue.sendAppMessage(regionalStats);
-//                 MessageQueue.sendAppMessage(countryStats);
-//                 MessageQueue.sendAppMessage(worldStats);
-//                 MessageQueue.sendAppMessage({"Ready":1});
-
-//             }
-//         }
-//     }
-//     req.send();
-
-// }
-
-//old papaparse functionality
-// function getCoronaData2(){
-//     Papa.parse('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
-//     {
-//         download:true,
-//         complete:(results)=>{
-//             var modified = [];
-//             for (let location of results.data){
-//                 var count = location[location.length-1];
-//                 if (count>minCases){
-//                     var locString = "";
-//                     if(location[0].length > 1){
-//                         locString = location[0] + ", " + location[1];
-//                     } else {
-//                         locString = location[1];
-//                     }
-//                     var loc = {
-//                         location:locString,
-//                         lat: location[2],
-//                         long: location[3],
-//                         distance: Math.round(distance(location[2], location[3],  yourLocation.lat, yourLocation.long, units)),
-//                         count:count
-//                     }
-//                     if(!locString.includes("Diamond Princess")){
-//                         modified.push(loc);
-//                     }
-//                 }
-//             }
-//             modified.sort((a, b) => (a.distance > b.distance) ? 1 : -1);
-//             console.log(JSON.stringify(modified[0]));
-//             var closestLocations = "";
-//             for (let i=0; i < 5; i++){
-//                 closestLocations += modified[i].location + "\n" + modified[i].distance+displayUnits(units)+", " + modified[i].count + " cases" + "\n" ;
-//             }
-//             var virusData = {
-//                 // "Distance": modified[0].distance+displayUnits(units),
-//                 "Scope": 0,
-//                 "Location": modified[0].location,
-//                 "Cases": modified[0].count + "",
-//                 "Deaths": 0+" ",
-//                 "Recovered": 0+" ",
-//                 "Ready": 1,
-//                 // "ClosestCases": closestLocations
-//             };
-//             console.log(JSON.stringify(virusData));
-//             // localStorage.setItem("lastSync", moment().format("MM-DD-YYYY"));
-//             localStorage.setItem("lastSync", new Date().getTime());
-//             localStorage.setItem("virusData", JSON.stringify(virusData));
-//             Pebble.sendAppMessage(virusData);
-//             // console.log(JSON.stringify(modified));
-//         },
-//         error:()=>{
-//             Pebble.showSimpleNotificationOnPebble("Error", "Error in parsing results from webserver.");
-//         }
-//     });
-// }
-
-
-// function distance(lat1, lon1, lat2, lon2, unit) {
-// 	if ((lat1 == lat2) && (lon1 == lon2)) {
-// 		return 0;
-// 	}
-// 	else {
-// 		var radlat1 = Math.PI * lat1/180;
-// 		var radlat2 = Math.PI * lat2/180;
-// 		var theta = lon1-lon2;
-// 		var radtheta = Math.PI * theta/180;
-// 		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-// 		if (dist > 1) {
-// 			dist = 1;
-// 		}
-// 		dist = Math.acos(dist);
-// 		dist = dist * 180/Math.PI;
-// 		dist = dist * 60 * 1.1515;
-// 		if (unit=="K") { dist = dist * 1.609344 }
-// 		if (unit=="N") { dist = dist * 0.8684 }
-// 		return dist;
-// 	}
-// }
-
-// function displayUnits(unit){
-//     switch(unit){
-//         case "M": return "mi";
-//         case "K": return "km";
-//         case "N": return "na";
-//     }
-// }
